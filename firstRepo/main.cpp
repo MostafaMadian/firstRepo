945851cf086dfd1d552b9c3e4b687befde15d3ce
MostafaMadian <mostafaibrahimmadian@gmail.com>
2024-03-29 06:54:38 +0200

    newChange

--- firstRepo/main.cpp
+++ firstRepo/main.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Abdo" << endl;
+    cout << "hello" << endl;
+    int x;
+    cin >> x;
+    cout << x << endl;
+    cout << "this is a new change on gitHub";
 }
