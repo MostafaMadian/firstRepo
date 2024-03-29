88afddc099e2c9a98beca76e7f313940f6a776cf
MostafaMadian <mostafaibrahimmadian@gmail.com>
2024-03-29 07:10:00 +0200

    newChange2
    test

--- firstRepo/main.cpp
+++ firstRepo/main.cpp
@@ -6,6 +6,5 @@ int main()
     cout << "hello" << endl;
     int x;
     cin >> x;
-    cout << x << endl;
-    cout << "this is a new change on gitHub";
+    cout << x << endl<<"this is a new change on gitHub";
 }
